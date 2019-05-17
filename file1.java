babae4785b368e0863c050aadb14e7c558b6dd6d
sumit8258 <smtsingh443@gmail.com>
2019-05-17 12:05:32 +0530

    file modified

--- file1.java
+++ file1.java
@@ -1,5 +1,5 @@
 class First {
   public static void main(String[] arguments) {
-    System.out.println("Let us do java programming.");
+    System.out.println("Let us understand some java programming.");
   }
 }
