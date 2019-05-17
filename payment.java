59f526ae68dacb5522732ba9439ce14645045b7b
sumit8258 <smtsingh443@gmail.com>
2019-05-17 13:58:16 +0530

    passing marks changed

--- payment.java
+++ payment.java
@@ -4,7 +4,7 @@ class IfElse {
   public static void main(String[] args) {
     int marksObtained, passingMarks;
    
-    passingMarks = 40;
+    passingMarks = 60;
    
     Scanner input = new Scanner(System.in);
    
