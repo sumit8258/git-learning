ca10102fa70c65a9dedcec9070f5a593cb58c7eb
sumit8258 <smtsingh443@gmail.com>
2019-05-17 13:37:48 +0530

    value of x and y initialised

--- add.java
+++ add.java
@@ -5,6 +5,8 @@ class AddNumbers
    public static void main(String args[])
    {
       int x, y, z;
+      int x=10
+      int y=20
  
       System.out.println("Enter two integers to calculate their sum");
       Scanner in = new Scanner(System.in);
