b45e08455dbbe394340959220692c202602ed1a9
sumit8258 <smtsingh443@gmail.com>
2019-05-17 09:52:52 +0530

    changes made in file1.java

--- file1.java
+++ file1.java
@@ -1,5 +1,5 @@
 class First {
   public static void main(String[] arguments) {
-    System.out.println("Let's do something using Java technology.");
+    System.out.println("Let us do java programming.");
   }
 }
